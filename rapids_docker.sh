4dbd1a983f48b0960adf0d479abaa49f45d059f5
Kshitiz Gupta <kshitizg@nvidia.com>
2022-06-27 18:19:53 -0700

    add xgb train at end of rapids prep

--- rapids_docker.sh
+++
@@ -1,6 +0,0 @@
-docker run --gpus all --rm -it \
-    --shm-size=1g --ulimit memlock=-1 \
-    -p 8887:8887 \
-    -v ${PWD}:/workspace \
-    -w /workspace \
-    rapidsai/rapidsai-core:22.06-cuda11.5-base-ubuntu20.04-py3.8
