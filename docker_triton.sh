463c1afbd4a1c428e2bb834e0f7179c06b6e57a6
Kshitiz Gupta <kshitizg@nvidia.com>
2022-07-12 18:13:45 -0700

    update lifecycle script to rapids 21.06 & add explanation for cpu prep conda env

--- docker_triton.sh
+++
@@ -1,6 +0,0 @@
-docker run --gpus all --rm -it \
-    --shm-size=1g --ulimit memlock=-1 \
-    -p 8888:8888 -p 8000:8000 -p 8001:8001 -p 8002:8002 \
-    -v ${PWD}:/workspace \
-    -w /workspace \
-    nvcr.io/nvidia/tritonserver:22.05-py3 bash
